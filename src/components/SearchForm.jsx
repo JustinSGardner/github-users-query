efd349ac77fef556e3a91ccabdfaccd057363b5f
Justin Gardner <the1jscott@gmail.com>
2020-10-13 22:10:03 -0500

    "Created components & completed SearchForm component"

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { loadData } from '../utils/loadData';
+
+class SearchForm extends Component {
+  state = {
+    users: [],
+    userName: '',
+  };
+
+  _handleChange = newText => {
+    this.setState({
+      userName: newText,
+    });
+  }
+
+  _handleSubmit = async () => {
+    const { userName } = this.state;
+    const newUserData = await loadData(
+      `https://api.github.com/users/${userName}`
+    );
+      this.setState({
+        users: [...this.state.users, newUserData]
+      });
+      console.log(this.state.users);
+    };
+  
+
+  render() {
+    return (
+      <>
+        <h1>Go on n findya a usr ferm Githeb!</h1>
+        <form>
+          <label>
+            <input 
+              type='text'
+              onChange={event => { this._handleChange(event.target.value)}}
+            />
+          </label>
+            <button 
+              type='button' 
+              onClick={this._handleSubmit}
+              >#gitThetTharPhoter!!
+            </button>
+        </form>
+      </>
+    )
+  }
+}
+
+export default SearchForm;
